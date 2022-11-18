8a140c731b7f71fa9c38328bbbfd17e354f5531c
Reuben <s021632@students.lmsd.org>
2022-11-18 13:03:53 -0500

    Initial Commit

---
+++ NewWaveStore/NewWaveStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreApp.swift
+//  NewWaveStore
+//
+//  Created by Reuben Gottesman (student LM) on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct NewWaveStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
